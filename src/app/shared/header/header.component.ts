0d15457669ccf9ad662420b1337c322033e37b8a
[Rosa][Arnau] <[rarnau@uoc.edu]>
2025-03-18 11:59:34 +0100

    cuarta

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,12 +10,13 @@ import { TranslateService } from '@ngx-translate/core';
 export class HeaderComponent {
   public activeLang = 'es';
 
-  constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang(this.activeLang);
-  }
+constructor(private translate: TranslateService) {
+  this.translate.setDefaultLang(this.activeLang);
+}
 
-  public cambiarLenguaje(lang: string) {
-    this.activeLang = lang;
-    this.translate.use(lang);
-  }
+public cambiarLenguaje(lang: string) {
+  this.activeLang = lang;
+  this.translate.use(lang);
+}
+ 
 }
